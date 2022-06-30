aa79928591d9cd0aee5fcc73a1d2a8a5b1a68527
anton_kurak <anton.kurak@gmail.com>
2022-06-30 12:50:21 +0300

    practise

--- main5.py
+++ main5.py
@@ -1,6 +1,6 @@
 class Numbers:
     def __init__(self, *args):
-        self.number = args
+        self.numbers = args
         self.i = -1
 
     def __iter__(self):
@@ -9,10 +9,10 @@ class Numbers:
     def __next__(self):
         self.i += 1
         if self.i < len(self.numbers):
-        return self.numbers[self.i]
+            return self.numbers[self.i]
         else:
-    self.i = -1
-        raise StopIteration
+            self.i = -1
+            raise StopIteration
 
 numbers = Numbers(1, 2, 3, 4, 5, 6, 7, 8, 9)
 for number in numbers:
