e0d7572a1370b195419e1b76cdfc959f27bd23a3
anton_kurak <anton.kurak@gmail.com>
2022-06-30 10:50:57 +0300

    hw

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-text = "hello---python---world"
-words = text.split("---")
-print(words)
+lst =   [1, 2, 3, 4, 5, 6 ,7, 8, 9, 10]
+n = int(input('numbs to shift:'))
+def shift(numbs_lst):
+    new_lst =   numbs_lst
+    counter =   0
+    while counter   !=n:
+        new_lst.insert(0, new_lst.pop())
+        counter +=  1
+        return new_lst
+    print(lst)
+    print(shift(lst))
